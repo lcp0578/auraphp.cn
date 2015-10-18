bb1e3e029268cbad5ab0fdab8623810df7a74984
李春鹏 <lcp0578@gmail.com>
2015-10-18 18:51:27 +0800

    翻译了auraphp的简介
    翻译了auraphp的简介

--- js/adjectives.js
+++ js/adjectives.js
@@ -1,16 +1,16 @@
 $(function ($) {
     var adjectives = [
-        "powerful",
-        "high-quality",
-        "high-performance",
-        "reliable",
-        "modern",
-        "advanced",
-        "professional"
+        "强有力",
+        "高质量",
+        "高性能",
+        "可信赖",
+        "现代化",
+        "高级",
+        "专业"
     ];
     var adjLower = adjectives[Math.floor(adjectives.length * Math.random())];
-    var adjUpper = adjLower[0].toUpperCase() + adjLower.slice(1);
+    // var adjUpper = adjLower[0].toUpperCase() + adjLower.slice(1);
     $('<span />', {
-        html: adjUpper + ' tools for ' + adjLower + ' applications.'
+        html: '为' + adjLower + '的应用程序而生的' + adjLower + '的工具'
     }).appendTo('#adjectives');
 });
